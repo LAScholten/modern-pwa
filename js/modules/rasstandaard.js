e43089d412d2039d4b2573583b276c420b6d8872
Leon Scholten <l.scholten20@gmail.com>
2026-01-29 19:41:45 +0100

    Update site

--- js/modules/rasstandaard.js
+++ js/modules/rasstandaard.js
@@ -12,7 +12,142 @@ const rasstandaardTranslations = {
     },
     de: {
         title: "Rassestandard",
-        content: "Rassestandard"
+        content: "17.02.2020 / DE  
+FCI - Standard Nr. 291   
+ 
+EURASIER 
+URSPRUNG: Deutschland. 
+DATUM DER PUBLIKATION DES GÜLTIGEN 
+OFFIZIELLEN STANDARDS: 04/09/2019. 
+VERWENDUNG: Begleithund. 
+KLASSIFIKATION FCI: Gruppe 5 Spitze und Hunde vom 
+Urtyp. 
+Sektion 5 Asiatische Spitze und  
+verwandte Rassen. 
+Ohne Arbeitsprüfung. 
+
+KURZER GESCHICHTLICHER ABRISS: 1960 entstand durch 
+Kreuzung von Chow-Chow und Wolfspitz eine Hunderasse, die 
+zunächst « Wolf-Chow » genannt wurde und nach Einkreuzung des 
+Samojeden 1973 in « Eurasier » umbenannt und von der F.C.I. 
+anerkannt wurde. Rassegründer war Julius Wipfel. 
+
+ALLGEMEINES 
+ERSCHEINUNGSBILD: 
+Harmonisch aufgebauter, mittelgroβer, spitzartiger Hund mit Stehohren in 
+verschiedenen Farbschlägen; mit einer Haarlänge, die die Proportionen des Körpers noch erkennen lässt; mittelschwerer Knochenbau. 
+
+WICHTIGE PROPORTIONEN: Die Rumpflänge beträgt etwas mehr als die Widerristhöhe. Das Verhältnis zwischen Länge des Fanges und Länge des Hirnschädels ist annähernd gleich. 
+
+VERHALTEN / CHARAKTER (WESEN): Selbstbewusst, ruhig, ausgeglichen mit hoher Reizschwelle, wach- und aufmerksam ohne lautfreudig zu sein; mit stark ausgeprägter Bindung an seine Familie; Fremden gegenüber gelassen und unaufdringlich. Zu vollen 
+Ausbildung dieser Merkmale braucht der Eurasier ständigen, engen häuslichen Kontakt zu seiner Familie und eine verständnisvolle, jedoch konsequente Erziehung. 
+
+KOPF: Harmonischer, nicht zu breiter Schädel, Kopfform von oben und von der Seite gesehen keilförmig, Nasenrücken und Hirnschädel parallel. 
+
+OBERKOPF:  
+Schädel: Flache Stirnpartie mit gut erkennbarer Stirnfurche; 
+ausgeprägter Hinterhauptstachel. 
+Stopp: Wenig ausgeprägt. 
+
+GESICHTSSCHÄDEL:  
+Nasenschwamm: Mittelgroβ, Nasenschwamm schwarz pigmentiert. 
+Fang: Weder zu grob noch zu spitz; sich nach vorn bis zum 
+Nasenschwamm verjüngend; gerader Nasenrücken und gerade Unterkieferäste. 
+Lefzen: Lippenränder straff und schwarz pigmentiert. 
+Kiefer / Zähne: Kiefer kräftig; breiter Unterkieferbogen. Kräftiges, 
+vollständiges Gebiss (42 Zähne gemäβ der Zahnformel); Zahnschluss entweder Schere oder Zange; obere Schneidezähne greifen entweder eng über untere Schneidezähne oder aufeinander; Prämolaren und Molaren in einer Linie ohne Lücken; alle Zähne senkrecht im Kiefer stehend. 
+Backen: Wenig betont. 
+
+AUGEN: Dunkel, mittelgroβ, nicht zu tiefliegend oder hervorquellend; leicht schräg gestellte Lidspalten; Lidränder schwarz pigmentiert und straff anliegend. 
+
+OHREN: Ca. eine Ohrbasisbreite voneinander angesetzt; mittelgroβ und dreieckig; Stehohr mit leicht abgerundeten Spitzen. 
+Ohrenspitzen und Mittelpunkt des Stirnabsatzes bilden ein nahezu gleichseitiges Dreieck. 
+HALS: Von mittlerer Länge, in Harmonie zum Gesamtbild, gut bemuskelt; straff anliegende Kehlhaut. Fliessender Übergang zum Körper. 
+
+KÖRPER:  
+Obere Profillinie: Kräftig, nicht zu kurz. 
+Widerrist: Betont. 
+Rücken: Fest und gerade; sehr gut bemuskelt. 
+Lenden: Von guter Länge und Breite, sehr gut bemuskelt. 
+Kruppe: Fast gerade, breit und kräftig. 
+Brust: Bis zu den Ellenbogen reichend; mit ovaler Rippenwölbung; 
+gut entwickelte Vorbrust, ohne jedoch betont zu sein; langes, weit nach hinten reichendem Brustbein. 
+Untere Profillinie und Bauch: Im Bauchbereich leicht aufgezogen; 
+Flanken leicht eingezogen. 
+RUTE: Hoher Ansatz, rund und fest, von guter Dicke, sich dem Ende zu verjüngend, bis zu den Sprunggelenken reichend; buschig behaart, aufliegend -nach vorn über den Rücken oder leicht seitwärts gebogen oder gerollt getragen. 
+
+GLIEDMASSEN  
+VORDERHAND: 
+Allgemeines: Von vorne gesehen gerade und parallel gestellt; von der Seite betrachtet gut gewinkelt; Ober- und Unterarm fast von gleicher Länge. 
+Schulter: Gut bemuskelt, Schulterblatt etwas schräg liegend. 
+Oberarm: Mittellang und gut bemuskelt. 
+Ellenbogen: Am Brustkorb anliegend. 
+Unterarm: Mittellang; gut bemuskelt. 
+Vordermittelfu: Mittellang, kräftig, von vorne gesehen völlig gerade; von der Seite gesehen leicht nach vorne gerichtet. 
+Vorderpfoten: Oval; eng an einander liegende, mäβig aufgewölbte 
+Zehen; kräftige, dunkel pigmentierte Krallen; feste, gut gepolsterte, schwarz pigmentierte Ballen. Dichte Behaarung zwischen den Ballen. 
+
+HINTERHAND: 
+Allgemeines: Von hinten gesehen gerade und parallel gestellt, von der Seite betrachtet gut gewinkelt. Ober- und Unterschenkel fast von gleicher Länge. 
+Becken: Leicht schräggelagert. 
+Oberschenkel: Mittellang und stark bemuskelt. 
+Knie: Stabil, Winkel nicht zu stark geöffnet. 
+Unterschenkel: Mittellang, gut bemuskelt. 
+Sprunggelenk: Nicht zu tief angesetzt, weder einwärts noch auswärts gerichtet, stabil, nicht nach vorne durchdrückend. 
+Hintermittelfu: Gut lang und breit; von der Seite betrachtet senkrecht gestellt. 
+Hinterpfoten: Oval, geschlossen, mäβig aufgewölbt; kräftige, dunkel pigmentierte 
+Zehennägel; feste, gut gepolsterte, schwarz pigmentierte Ballen. Dichte Behaarung zwischen den Ballen. 
+
+GANGWERK: Raumgreifend und harmonisch mit kräftigem Schub und gutem Vortritt. Rücken in der Bewegung fest und in guter Gleichgewichtslage; Vorder- und Hinterhand gerade tretend, bei zunehmender Laufgeschwindigkeit enger werdend. 
+Bevorzugte Gangart: Trab. 
+
+HAUT: Straff; pigmentiert. 
+
+HAARKLEID  
+Haar: Am ganzen Körper dichte Unterwolle und mittellanges, lose anliegendes Deckhaar; Fang, Gesicht, Ohren und Vorderseite der Läufe kurzhaarig; Rute, Rückseite der Vorderläufe (Fahnen) und Hinterläufe (Hosen) länger behaart. Deckhaar am Hals nur mäβig länger als am Körper, keine Mähne bildend. 
+Farbe: Alle Farben und Farbkombinationen sind zugelassen, ausgenommen reinweiβ, gescheckt. 
+
+GRÖSSE UND GEWICHT: 
+Rüden:  52 - 60 cm, 22 - 30 kg,
+Hündinnen:  48 - 56 cm, 18 - 26 kg.
+Maβgebend ist die Ausgewogenheit der Proportionen, doch sind die anzustrebenden 
+Idealwerte Gewichtsangaben.
+
+FEHLER: Jede Abweichung von den vorgenannten Punkten muss als Fehler angesehen werden, dessen Bewertung in genauem Verhältnis zum Grad der Abweichung stehen sollte und dessen Einfluss auf die Gesundheit und das Wohlbefinden des Hundes zu 
+beachten ist. 
+
+SCHWERE FEHLER: 
+• Starke Pigmentverluste. 
+• Starker Stirnabsatz und/oder stark gewölbter Oberkopf. 
+• Steile Hinterhand. 
+• Stark nachgebendes Vorderfuβwurzelgelenk. 
+• Senk- und Karpfenrücken. 
+• Verlust von drei oder vier P2. 
+• Verlust eines oder mehrerer P1 sowie gleichzeitig zwei oder 
+mehrerer P2. 
+• Sichtlich offene, nicht aufliegende Rute. 
+• Kurze Rute. 
+• Mangelndes Geschlechtsgepräge. 
+
+DISQUALIFIZIERENDE FEHLER: 
+• Aggressive oder űbermäβig ängstliche Hunde 
+• Hunde, die deutlich physische Abnormalitäten oder 
+Verhaltensstőrungen aufweisen. 
+• Fehlen eines Schneide- oder Fangszahns, auch mehrerer. 
+• Fehlen eines oder mehrerer Prämolaren 3 oder 4. 
+• Fehlen eines oder mehrerer Molaren 1 oder 2. 
+• Gebissanomalien. 
+• Ektropium, Entropium; zu tief liegende oder zu kleine Augen. 
+• Kippohren, Hängeohren. 
+• Knickrute. 
+• Unstabiles Sprunggelenk: abnormale Beweglichkeit nach vorne oder zur Seite beim stehenden Hund. 
+• Scheckenbildung. 
+• Reinweiβ. 
+
+N.B. 
+• Rüden müssen zwei offensichtlich normal entwickelte Hoden aufweisen, die sich vollständig im Hodensack befinden. 
+• Zur Zucht sollen ausschließlich funktional und klinisch gesunde, rassetypische Hunde verwendet werden.
+"
     }
 };
 
