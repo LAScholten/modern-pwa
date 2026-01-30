897c725fb74c1119d98b9221e89acfc8e6ac2cfa
Leon Scholten <l.scholten20@gmail.com>
2026-01-30 21:16:55 +0100

    Update site

--- js/modules/gezondheid.js
+++ js/modules/gezondheid.js
@@ -6,7 +6,26 @@ const gezondheidTranslations = {
         title: "Gezondheid",
         content: `
             <div class="gezondheid-content">
-                <p>GEZONDHEID</p>
+                <h3>Algemeen</h3>
+                <p>Over het algemeen genomen is de Eurasier een gezond ras met een gemiddelde levensverwachting van tussen de 12 en 14 jaar. Toch is een leeftijd van 16 jaar geen uitzondering en zijn er ook Eurasiers bekend die de respectabele leeftijd van 18 jaar hebben behaald. Aangezien we het over levende dieren hebben is het zo dat je ten alle tijde te maken kan hebben met afwijkingen die al dan niet erfelijk belast zijn. De meeste Europese landen onderzoeken op de volgende afwijkingen: Heupdysplasie (HD), Patella Luxatie (PL), oogafwijkingen, schildklierafwijkingen en het Dandy Walker Syndroom. In mindere mate wordt in diverse landen ook nog onderzocht op Elleboogdysplasie (ED).</p>
+                
+                <h3>Heupdysplasie</h3>
+                <p>Binnen de Eurasier komt heupdysplasie voor. Deze afwijking is voor ongeveer 20 á 30% erfelijk belast en hangt daarnaast af van omgevingsfactoren. De gradaties van vrij van HD tot zware aantasting zijn HD-A(vrij), HD-B, HD-C, HD-D, HD-E. Op het moment van schrijven is ongeveer 90% van de uitslagen HD-A. HD-A is de best mogelijke uitslag. Dit betekent een zeer goede heupkop en heupkom die goed aan elkaar aansluiten.</p>
+                
+                <h3>Patella Luxatie</h3>
+                <p>De patella is de knieschijf die in een kraakbeensleuf ligt. Bij patella luxatie schiet de knieschijf uit de sleuf naar binnen of naar buiten. Dit is niet de bedoeling en daarbij ook zeer pijnlijk voor de hond. Het lopen zal bij een te hoge gradatie ook ernstig belemmerd worden. De gradaties zijn PL0(vrij), PL1, PL2, PL3, PL4. Op het moment van schrijven is bij de Eurasier ongeveer 94% van de onderzochte honden PL0.</p>
+                
+                <h3>Oogonderzoek</h3>
+                <p>In Nederland biedt het ECVO oogonderzoek een uitgebreide examinatie naar maar liefst 16 verschillende oogafwijkingen bij de hond. In andere landen in Europa is dit onderzoek minder gangbaar en wordt er vaak alleen gezocht naar de oogafwijking distichiasis. Deze afwijking houdt in dat zich op het ooglid van de hond één of enkele naar binnen wijzende haartjes bevinden. Indien deze haartjes zacht zijn kunnen ze weinig schade aanrichten. Op het moment dat deze haartjes harder zijn kunnen deze irriteren en schade toebrengen aan de oogbol. Hoewel dit van tijdelijke aard kan zijn blijft de eenmaal bevonden diagnose distichiasis levenslang staan. Op het moment van schrijven is bij onderzochte Eurasiers tot nu toe ongeveer 70% vrij van distichiasis. Honden die wel distichiasis hebben zijn niet uitgesloten voor de fokkerij, echter zal de combinatie altijd gemaakt moeten worden met een hond die vrij is van distichiasis.</p>
+                
+                <h3>Schildklieronderzoek</h3>
+                <p>Bij honden betekent een afwijking aan de schildklier dat de schildklier te traag werkt. Deze afwijking behoort tot de auto-immuunziektes. De meest voorkomende symptomen van schildklierproblemen zijn sloomheid, zwaarlijvigheid, stemmingswisselingen, mankheid en vachtproblemen. Nagenoeg elk Europees land vraagt onderzoek op schildklierafwijkingen. Onderzocht wordt de TSH (Thyrod Stimulating Hormone), T4 (Thyroxine), Free T4 (Free Thyroxine) en TGAA (Thyroglobulin Autoantibody). De test wordt beïnvloed door diverse factoren zoals stress, type voer en de lengte van de cyclus bij de teef. De uitslag voor dit onderzoek wordt door de diverse Europese rasverenigingen anders in behandeling genomen. Wel zorgt men er altijd voor dat één van de ouders in alle waarden een goede uitslag heeft.</p>
+                
+                <h3>Dandy Walker Syndroom</h3>
+                <p>Het Dandy Walker Syndroom is een aangeboren afwijking in de hersenen. Het wordt gekenmerkt door een verkleind, onderontwikkeld cerebellum en cysten in het vierde ventrikel. De duidelijkste symptomen zijn een vertraagde ontwikkeling van de motorische functies en een progressief toenemende grootte van de schedel. Deze afwijking is al vroeg bij de pups te zien. Pups die niet goed op hun poten kunnen staan en een dronken indruk geven zijn duidelijke signalen. Sinds een aantal jaren wordt op het Dandy Walker Syndroom getest door middel van een DNA test. Er wordt niet gefokt met honden die dit syndroom hebben of dragers zijn van dit syndroom. Binnen de Eurasier is dit syndroom in het verleden voorgekomen.</p>
+                
+                <h3>Elleboogdysplasie</h3>
+                <p>Het onderzoek naar elleboogdysplasie gebeurt door middel van röntgenfoto's. Daarbij wordt de elleboog op de volgende afwijkingen gecontroleerd: artrose, LPC (los processus coronoideus), OCD (osteochondrose dissecans), LPA (los processus anconeus) en INC (incongruentie van de gewrichtsvlakken). De eindbeoordeling wordt in de volgende gradaties aangegeven: ED0(vrij), ED1, ED2, ED3. In Nederland wordt dit meestal niet onderzocht, in sommige andere Europese landen weer wel. Deze landen hanteren over het algemeen de regel dat tot op een bepaalde hoogte met honden die ED hebben gefokt mag worden maar waarbij één van de ouders vrij van ED moet zijn.</p>
             </div>
         `
     },
@@ -14,7 +33,26 @@ const gezondheidTranslations = {
         title: "Health",
         content: `
             <div class="gezondheid-content">
-                <p>HEALTH</p>
+                <h3>General</h3>
+                <p>Generally speaking, the Eurasier is a healthy breed with an average life expectancy between 12 and 14 years. However, reaching 16 years of age is not exceptional, and there are known Eurasiers who have reached the respectable age of 18 years. Since we are dealing with living animals, it's possible to encounter abnormalities at any time, which may or may not be hereditary. Most European countries investigate the following abnormalities: Hip Dysplasia (HD), Patella Luxation (PL), eye abnormalities, thyroid abnormalities, and Dandy Walker Syndrome. To a lesser extent, various countries also investigate Elbow Dysplasia (ED).</p>
+                
+                <h3>Hip Dysplasia</h3>
+                <p>Hip dysplasia occurs in the Eurasier breed. This abnormality is approximately 20-30% hereditary and also depends on environmental factors. The grades range from HD-free to severe: HD-A (free), HD-B, HD-C, HD-D, HD-E. At the time of writing, about 90% of the results are HD-A. HD-A is the best possible result, indicating an excellent hip ball and socket that fit together well.</p>
+                
+                <h3>Patella Luxation</h3>
+                <p>The patella is the kneecap that sits in a cartilage groove. In patella luxation, the kneecap slips out of the groove either inward or outward. This is not intended and is also very painful for the dog. Walking will also be seriously impaired at higher grades. The grades are PL0 (free), PL1, PL2, PL3, PL4. At the time of writing, about 94% of examined Eurasiers are PL0.</p>
+                
+                <h3>Eye Examination</h3>
+                <p>In the Netherlands, the ECVO eye examination provides a comprehensive examination of as many as 16 different eye abnormalities in dogs. In other European countries, this examination is less common and often only looks for the eye abnormality distichiasis. This abnormality means that there are one or several inwardly pointing hairs on the dog's eyelid. If these hairs are soft, they can cause little damage. When these hairs are harder, they can irritate and damage the eyeball. Although this may be temporary, once diagnosed with distichiasis, the diagnosis remains for life. At the time of writing, about 70% of examined Eurasiers are free of distichiasis. Dogs with distichiasis are not excluded from breeding, but they must always be paired with a dog that is free of distichiasis.</p>
+                
+                <h3>Thyroid Examination</h3>
+                <p>In dogs, a thyroid abnormality means the thyroid gland is underactive. This abnormality belongs to autoimmune diseases. The most common symptoms of thyroid problems are lethargy, obesity, mood swings, lameness, and coat problems. Virtually every European country requires examination for thyroid abnormalities. Tests include TSH (Thyroid Stimulating Hormone), T4 (Thyroxine), Free T4 (Free Thyroxine), and TGAA (Thyroglobulin Autoantibody). The test is influenced by various factors such as stress, type of food, and the length of the cycle in females. Different European breed clubs handle the results of this examination differently. However, it's always ensured that one parent has good results in all values.</p>
+                
+                <h3>Dandy Walker Syndrome</h3>
+                <p>Dandy Walker Syndrome is a congenital brain abnormality. It's characterized by a reduced, underdeveloped cerebellum and cysts in the fourth ventricle. The most obvious symptoms are delayed development of motor functions and progressively increasing skull size. This abnormality can be seen early in puppies. Puppies that cannot stand properly on their legs and give a drunken impression are clear signals. For several years, Dandy Walker Syndrome has been tested through DNA testing. Dogs with this syndrome or carriers of this syndrome are not bred. This syndrome has occurred in the Eurasier breed in the past.</p>
+                
+                <h3>Elbow Dysplasia</h3>
+                <p>The examination for elbow dysplasia is done through X-rays. The elbow is checked for the following abnormalities: arthritis, LPC (loose coronoid process), OCD (osteochondrosis dissecans), LPA (loose anconeal process), and INC (incongruence of joint surfaces). The final assessment is given in the following grades: ED0 (free), ED1, ED2, ED3. This is usually not examined in the Netherlands, but it is in some other European countries. These countries generally follow the rule that dogs with ED can be bred up to a certain extent, provided one parent is free of ED.</p>
             </div>
         `
     },
@@ -22,7 +60,26 @@ const gezondheidTranslations = {
         title: "Gesundheit",
         content: `
             <div class="gezondheid-content">
-                <p>GESUNDHEIT</p>
+                <h3>Allgemein</h3>
+                <p>Generell handelt es sich beim Eurasier um eine gesunde Rasse mit einer durchschnittlichen Lebenserwartung zwischen 12 und 14 Jahren. Allerdings stellt ein Alter von 16 Jahren keine Ausnahme dar und es sind auch Eurasier bekannt, die das respektable Alter von 18 Jahren erreicht haben. Da es sich um lebende Tiere handelt, kann es immer wieder zu Anomalien kommen, die erblich bedingt sein können oder auch nicht. Die meisten europäischen Länder untersuchen die folgenden Anomalien: Hüftdysplasie (HD), Patellaluxation (PL), Augenanomalien, Schilddrüsenanomalien und das Dandy-Walker-like-Malformation Syndrom. In geringerem Umfang wird in verschiedenen Ländern auch die Ellenbogendysplasie (ED) untersucht.</p>
+                
+                <h3>Hüftdysplasie</h3>
+                <p>Hüftdysplasie kommt beim Eurasier vor. Diese Abweichung ist zu etwa 20 bis 30 % erblich bedingt und hängt auch von Umweltfaktoren ab. Die Abstufungen von HD-frei bis schwere HD sind HD-A (frei), HD-B, HD-C, HD-D, HD-E. Zum Zeitpunkt des Verfassens dieses Artikels handelt es sich bei etwa 90 % der Ergebnisse um HD-A. HD-A ist das bestmögliche Ergebnis. Das bedeutet, dass Hüftkopf und Hüftpfanne sehr gut zusammenpassen.</p>
+                
+                <h3>Patellaluxation</h3>
+                <p>Die Patella ist die Kniescheibe, die in einer Knorpelrille liegt. Bei einer Patellaluxation rutscht die Kniescheibe aus ihrem Schlitz heraus oder hinein. Dies ist nicht beabsichtigt und für den Hund zudem sehr schmerzhaft. Auch das Gehen wird bei einem zu hohen Grad erheblich beeinträchtigt. Die Einstufung erfolgt in Grad PL0 (frei), PL1, PL2, PL3, PL4. Zum Zeitpunkt des Verfassens dieses Artikels sind etwa 94 % der untersuchten Eurasier PL0.</p>
+                
+                <h3>Augenuntersuchung</h3>
+                <p>In den Niederlanden bietet die ECVO-Augenuntersuchung eine umfassende Untersuchung von nicht weniger als 16 verschiedenen Augenanomalien bei Hunden. In anderen Ländern Europas ist diese Untersuchung seltener und oft wird nur nach der Augenerkrankung Distichiasis gesucht. Diese Anomalie bedeutet, dass sich auf dem Augenlid des Hundes ein oder mehrere nach innen gerichtete Haare befinden. Wenn diese Haare weich sind, können sie kaum Schaden anrichten. Wenn diese Haare härter sind, können sie den Augapfel reizen und schädigen. Auch wenn dies vorübergehend sein kann, bleibt die einmal gestellte Diagnose einer Distichiasis lebenslang bestehen. Zum Zeitpunkt des Schreibens sind etwa 70 % der untersuchten Eurasier bisher frei von Distichiasis. Hunde, die Distichiasis haben, sind nicht von der Zucht ausgeschlossen, die Verpaarung muss jedoch immer mit einem Hund erfolgen, der frei von Distichiasis ist.</p>
+                
+                <h3>Schilddrüsenuntersuchung</h3>
+                <p>Bei Hunden bedeutet eine Anomalie der Schilddrüse, dass die Schilddrüse zu langsam arbeitet. Diese Anomalie zählt zu den Autoimmunerkrankungen. Die häufigsten Symptome von Schilddrüsenproblemen sind Lethargie, Fettleibigkeit, Stimmungsschwankungen, Lahmheit und Fellprobleme. In fast jedem europäischen Land ist eine Untersuchung auf Schilddrüsenanomalien vorgeschrieben. Untersucht werden TSH (Schilddrüsenstimulierendes Hormon), T4 (Thyroxin), freies T4 (freies Thyroxin) und TGAA (Thyreoglobulin-Auto-Antikörper). Der Test wird von verschiedenen Faktoren wie Stress, Art der Nahrung und der Zykluslänge der Hündin beeinflusst. Die Ergebnisse dieser Prüfung werden von den verschiedenen europäischen Zuchtverbänden unterschiedlich behandelt. Es wird jedoch immer darauf geachtet, dass sich ein Elternteil in allen Werten im unauffälligen Bereich befindet.</p>
+                
+                <h3>Dandy-Walker-like-Malformation(DWLM) Syndrom</h3>
+                <p>Das Dandy-Walker-like-Malformation Syndrom ist eine angeborene Anomalie des Gehirns. Es ist durch ein reduziertes, unterentwickeltes Kleinhirn und Zysten im vierten Ventrikel gekennzeichnet. Die offensichtlichsten Symptome sind eine verzögerte Entwicklung motorischer functies und eine zunehmende Schädelgröße. Diese Anomalie kann schon früh bei Welpen beobachtet werden. Ein deutliches Signal sind Welpen, die nicht richtig auf den Beinen stehen können und betrunken wirken. Auf das Dandy-Walker-like-Malformation Syndrom wird seit einigen Jahren mithilfe eines DNA-Tests getestet. Mit Hunden, die an diesem Syndrom leiden, wird seitdem nicht mehr gezüchtet. Dieses Syndrom kam in der Vergangenheit beim Eurasier vor.</p>
+                
+                <h3>Ellenbogendysplasie</h3>
+                <p>Die Untersuchung auf Ellenbogendysplasie erfolgt mittels Röntgen. Der Ellenbogen wird auf folgende Anomalien untersucht: Arthrose, LPC (lockerer Processus coronoideus), OCD (Osteochondrosis dissecans), LPA (lockerer Processus anconeus) und INC (Inkongruenz der Gelenkflächen). Die Abschlussbewertung wird in folgenden Abstufungen angegeben: ED0(frei), ED1, ED2, ED3. Dies wird in den Niederlanden normalerweise nicht untersucht, in einigen anderen europäischen Ländern jedoch schon. In diesen Ländern gilt im Allgemeinen die Regel, dass mit Hunden mit ED bis zu einem gewissen Grad gezüchtet werden darf, ein Elternteil jedoch frei von ED sein muss.</p>
             </div>
         `
     }
