b29f8da7a113b11f5e47f943228f5247bd798d3b
Leon Scholten <l.scholten20@gmail.com>
2026-02-04 22:07:24 +0100

    Update site

--- js/modules/informatie.js
+++ js/modules/informatie.js
@@ -19,7 +19,11 @@ class InformatieManager {
                 infoTitle: 'Module nog in ontwikkeling',
                 infoText: 'Deze module toont straks informatie over de werkwijze binnen de app.',
                 sectionTitle: 'De werkwijze module',
-                sectionText: 'Hier komt informatie over de werkwijze binnen de app.',
+                sectionText: `Werkwijze:
+
+Gebruiker: 
+Een gebruiker kan zoeken naar een hond door in het menu op Hond zoeken te klikken en de naam van de hond in te typen. Als de hond in het dropdown menu verschijnt dan klik je de hond aan in het dropdown menu en komt het scherm naar voren met de ouders en gezondheid gegevens. Ook staat daar de knop "Stamboom" als je hier op klik genereerd het programma de stamboom van deze hond. In de stamboom kan je op een card van een hond klikken waardoor je een popup krijgt met de gegevens van de betreffende hond. Als er foto's van de hond zijn staan deze in het klein ook hierin. Op het moment dat je op een kleine foto klik wordt deze vergroot. 
+In de fotogalerij kan je alle foto's laden en door de foto's scrollen en een foto vergroten om er op te klikken.`,
                 closeBtn: 'Sluiten'
             },
             en: {
@@ -27,7 +31,11 @@ class InformatieManager {
                 infoTitle: 'Module still in development',
                 infoText: 'This module will soon show information about the workflow within the app.',
                 sectionTitle: 'The Workflow module',
-                sectionText: 'Information about the workflow within the app will appear here.',
+                sectionText: `Workflow:
+
+User:
+A user can search for a dog by clicking on "Search for dog" in the menu and typing the name of the dog. If the dog appears in the dropdown menu, you click on the dog in the dropdown menu and the screen will appear with the parent and health data. There is also the "Pedigree" button; if you click on this, the program generates the pedigree of this dog. In the pedigree you can click on a dog's card which gives you a popup with the details of that particular dog. If there are photos of the dog, these are also displayed in small size here. When you click on a small photo, it will be enlarged.
+In the photo gallery you can load all photos, scroll through the photos and enlarge a photo to click on it.`,
                 closeBtn: 'Close'
             },
             de: {
@@ -35,7 +43,11 @@ class InformatieManager {
                 infoTitle: 'Modul noch in Entwicklung',
                 infoText: 'Dieses Modul wird bald Informationen über die Arbeitsweise innerhalb der App anzeigen.',
                 sectionTitle: 'Das Arbeitsweise-Modul',
-                sectionText: 'Hier erscheinen Informationen über die Arbeitsweise innerhalb der App.',
+                sectionText: `Arbeitsweise:
+
+Benutzer:
+Ein Benutzer kann nach einem Hund suchen, indem er im Menü auf "Hund suchen" klickt und den Namen des Hundes eingibt. Wenn der Hund im Dropdown-Menü erscheint, klicken Sie den Hund im Dropdown-Menü an und der Bildschirm erscheint mit den Eltern- und Gesundheitsdaten. Dort befindet sich auch die Schaltfläche "Stammbaum". Wenn Sie darauf klicken, generiert das Programm den Stammbaum dieses Hundes. Im Stammbaum können Sie auf eine Karte eines Hundes klicken, wodurch Sie ein Popup mit den Daten des betreffenden Hundes erhalten. Wenn es Fotos des Hundes gibt, sind diese hier ebenfalls in kleiner Form enthalten. Wenn Sie auf ein kleines Foto klicken, wird es vergrößert.
+In der Fotogalerie können Sie alle Fotos laden, durch die Fotos scrollen und ein Foto vergrößern, um darauf zu klicken.`,
                 closeBtn: 'Schließen'
             }
         };
@@ -72,7 +84,7 @@ class InformatieManager {
                             <div class="text-center py-4">
                                 <i class="bi bi-gear display-1 text-info"></i>
                                 <h4 class="mt-3">${this.getText('sectionTitle')}</h4>
-                                <p class="text-muted">${this.getText('sectionText')}</p>
+                                <p class="text-muted" style="white-space: pre-line">${this.getText('sectionText')}</p>
                             </div>
                         </div>
                         <div class="modal-footer">
